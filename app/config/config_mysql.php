070be5411bc65b6ffbac0a3e4e2f80dd666277b9
Emil Sjunnesson <byrhult@gmail.com>
2014-12-01 11:24:25 +0100

    local config

--- app/config/config_mysql.php
+++ app/config/config_mysql.php
@@ -2,8 +2,8 @@
 
 return [
     'dsn'     => "mysql:host=blu-ray.student.bth.se;dbname=emsf14;", //blu-ray.student.bth.se
-    'username'        => "emsf14",
-    'password'        => "p,$75}jH", //p,$75}jH
+    'username'        => "root",
+    'password'        => "", //p,$75}jH
     'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
     'table_prefix'    => "phpmvc_",
     //'verbose' => true,
