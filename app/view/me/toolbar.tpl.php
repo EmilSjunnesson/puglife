d14ac8eb3743f20aee8184c825684595180c8a07
Emil Sjunnesson <byrhult@gmail.com>
2014-12-03 16:08:10 +0100

    changed toolbar link

--- app/view/me/toolbar.tpl.php
+++ app/view/me/toolbar.tpl.php
@@ -1,6 +1,6 @@
 <div class="contain">
 <?php if($this->users->isLoggedIn()): ?>
-<a href="<?=$this->url->create('users/id/' . $this->session->get('userId'))?>"><?=$this->users->Find($this->session->get('userId'))->name?></a> |
+<a href="<?=$this->url->create('users/id')?>"><?=$this->users->Find($this->session->get('userId'))->name?></a> |
  <a href="<?=$this->url->create('users/logout')?>">Logga ut</a>
 <?php else: ?> 
 <a href="<?=$this->url->create('users/login')?>">Logga in</a> |
